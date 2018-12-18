db8dff8d5b8f240d0190e1371eb9c56220439fe6
tshanmukh <tshanmukh713@gmail.com>
2018-12-18 20:22:30 +0530

    Updated the script to work incase of both snmp and Tl1

--- pluginimpl/Misc/Tl1MetricsList.py
+++ pluginimpl/Misc/Tl1MetricsList.py
@@ -8,7 +8,10 @@ from lxml import etree
 
 
 def printfields(id, templateName): # templateName is pm-template name and id is the metric-group id in it
-    """ Prints the details of all metrics by searching all the pm files in a profile xml"""
+    """ Prints the details of all metrics by searching all the pm files in a profile xml
+        id is a list which contains all the metric groupids of a specific template
+        templateName is the name of the pm-template
+    """
     if pmFiles:
         for i in pmFiles:
             # pmfile = getParser("/home/sthummala/workspace/vsure/centina/sa/profiles/" + i)
@@ -19,25 +22,30 @@ def printfields(id, templateName): # templateName is pm-template name and id is 
                 if groupId.get("id") in id:
                     metric = groupId.findall("metric")
                     parameter=groupId.find
-                    temp=templateName.split(r'/')
+                    temp=templateName.split(r'/')   #getting the template name to create the results file
                     temp1=temp[10].split(r'.')
                     name=temp1[0]
-                    f=open("results/"+name+".csv","a")
-                    # with open("results/"+name+".csv","w") as f:
+                    f=open("results/"+name+".csv","a") #opening file with template name in append mode
                     try:
                         for m in metric:
-                            f.write(m.get("name")+","+m.find("parameter").get("oid")+","+m.get("location")+","+m.get("direction")+"\n")
+                                f.write(str(groupId.get("id"))+","+str(m.get("name"))+","+str(m.find("parameter").get("oid"))+","+str(m.get("location"))+","+str(m.get("direction"))+"\n")
                     except:
-                        print("here")
+                        print("Exception while looping through the metricgroup id",str(groupId.get("id")))
+                        exit()
+                    finally:
+                        f.close()
     else:
         print("No pm xml files present while parsing" + templateName)
 
 
 pmFiles = []
 metricGroupId = []
-profileXml = '/home/sthummala/workspace/vsure/centina/sa/profiles/fujitsu-flashwave-4100es-r10.xml'
+pluginName=input("Please enter the plugin name: ")
+profileXml = '/home/sthummala/workspace/vsure/centina/sa/profiles/'+pluginName+'.xml'
 parser = etree.XMLParser(strip_cdata=False)
 root = etree.parse(profileXml, parser)
+meta=root.find("meta")
+protocol=meta.find("protocol").get("name")
 dependencies = root.find("dependencies")
 
 file = dependencies.findall("file")
@@ -45,8 +53,8 @@ for a in file:
     if a.get("path").startswith("pm/templates"):
         if a.get("path").endswith(".dtd"):
             continue
-        pmtemplate = "/home/sthummala/workspace/vsure/centina/sa/profiles/" + a.get("path").rstrip()
-        print(pmtemplate)
+        pmtemplate = "/home/sthummala/workspace/vsure/centina/sa/profiles/" + a.get("path")
+        print("Found pm template ",pmtemplate)
         # try:
         # template = getParser(pmtemplate,"template")
         parsertemplate = etree.XMLParser(strip_cdata=False)
@@ -55,8 +63,8 @@ for a in file:
         pmmetricgroups = perftemplate.find("pm-metric-groups")
         for i in pmmetricgroups.findall("value"):
             metricGroupId = metricGroupId + [i.text]
-        print(metricGroupId)
-        printfields(metricGroupId, pmtemplate)
+        print("Metric Groups are",metricGroupId)
+        printfields(metricGroupId, pmtemplate)   #Function prints all the parms for the id's passed to it looping in the PM xml paths added in the profile xml
         metricGroupId = []
 
     # except:
