0fff72195181361e8a6775cb6c3b5a8d35e95c97
tshanmukh <tshanmukh713@gmail.com>
2019-02-20 13:40:57 +0530

    Created a class to oid generation function

--- pluginimpl/OIDautomation/oid.py
+++ pluginimpl/OIDautomation/oid.py
@@ -1,5 +1,3 @@
-from typing import List, Any, Union
-
 __author__ = 'Shanmukh'
 
 import sys
@@ -8,95 +6,66 @@ import xml.dom.minidom
 from lxml import etree
 
 
-# Function to generate the OID
-def get_oid(doc, col):
-    """
-    :param col: colomn which requires the oid
-    :param doc: The mib repository object
-    :return result: returns all possible oid's of col
-    """
-    result=[]  # type: List[Union[str, Any]]
-    entry = doc.getElementsByTagName("entry")
-    for id in entry:
-        arg = ''
+class oid:
 
-        if id.getAttribute("name") == col:
-            # print("suffix: ", id.getAttribute("id"), "colomn: ", id.getAttribute("name"), "\n")
-            suffix=id.getAttribute("id")
-            ancestors = []
-            value = id.getAttribute("id")
-            ancestors.append(int(value))
-            parent = id.parentNode
-            while parent != None:
-                try:
-                    id = parent.getAttribute("id")
-                except:
-                    pass
-                try:
-                    ancestors.append(int(id))
-                except:
-                    pass
-                parent = parent.parentNode
-            OID = ancestors[::-1]
+    def get_oid(self, doc, col):
+        """
+        :param col: colomn which requires the oid
+        :param doc: The mib repository object
+        :return result: returns all possible oid's of col
+        """
+        doc = xml.dom.minidom.parse(doc)
+        result = []
+        entry = doc.getElementsByTagName("entry")
+        suffix = ''
+        for id in entry:
+            arg = ''
 
-            for i in OID:
-                arg = arg + "."
-                arg = arg + str(i)
-            result.append(arg)
+            if id.getAttribute("name") == col:
+                suffix = id.getAttribute("id")
+                ancestors = []
+                value = id.getAttribute("id")
+                ancestors.append(int(value))
+                parent = id.parentNode
+                while parent is not None:
+                    try:
+                        id = parent.getAttribute("id")
+                    except:
+                        pass
+                    try:
+                        ancestors.append(int(id))
+                    except:
+                        pass
+                    parent = parent.parentNode
+                OID = ancestors[::-1]
 
-    if len(result) != 0:
-        try:
-            print (suffix)
-            return result
-        except:
-            print('No match for ',col)
-    else:
-        print("Didn't find a match for \"", col, "\"")
+                for i in OID:
+                    arg = arg + "."
+                    arg = arg + str(i)
+                result.append(arg)
 
-
-# Global Initializations
-result = []
-path='/home/sthummala/workspace/repo/centina/sa/profiles/'
-
-# prompts and stores mib-repo name
-# mib_repository = input('Enter the mib-repository name: ')
-
-# @todo get the mib-repository name from the user
-# Parsing using dom
-doc = xml.dom.minidom.parse("eltek-mib-repository.xml")
-
-# searchString = input()
-
-oid = get_oid(doc, 'alarmUserConfigurable8Trap')
-
-print("OID: ", oid)
-
-profileXml='/home/sthummala/workspace/repo/centina/sa/profiles/eltek.xml'
-parser = etree.XMLParser(strip_cdata=False)
-root = etree.parse(profileXml, parser)
-
-dependency=root.find('dependencies')
-
-for i in dependency.findall('file'):
-    temp=i.get('path')
-    if 'snmp/inventory/' in temp:
-        if '.dtd' not in temp and 'if-mib' not in temp and 'pm/templates/' not in temp:
-            parser=etree.XMLParser(strip_cdata=False)
-            inventoryFile=etree.parse(path+temp,parser)
-            table=inventoryFile.findall('table')
-            for i in table:
-                print (i.get('name'))
-                print (get_oid(doc, i.get('name')))
-            # print (temp)
-    if 'pm' in temp:
-        if '.dtd' not in temp and 'if-mib' not in temp and 'pm/templates/' not in temp:
-            pmRoot = etree.parse(path+temp, parser)
-            for mg in pmRoot.iter('metricGroup'):
-                for met in mg.findall('parameter'):
-                    met.attrib['oid']=get_oid(doc, met.get('name'))
-
-                    print (met.get('oid') )
+        if len(result) != 0:
+            try:
+                # print(suffix)
+                return result
+            except:
+                print('No match for ', col)
+        else:
+            # print("Didn't find a match for \"", col, "\"")
+            return None
 
 
+if __name__ == '__main__':
+    # Global Initializations
+    result = []
+    path = '/home/sthummala/workspace/repo/centina/sa/profiles/'
 
+    # prompts and stores mib-repo name
+    mib_repository = input('Enter the mib-repository name: ')
 
+    searchString = input('Enter the object name')
+    obj = oid()
+
+    oid = obj.get_oid("eltek-mib-repository.xml", str(searchString))
+
+    print("OID: ", oid)
