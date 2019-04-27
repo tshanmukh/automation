100ce9a4eec94232af24820d4e4fcd85bb33f352
tshanmukh <tshanmukh713@gmail.com>
2019-04-27 15:35:53 +0530

    Update to create templates from new dictionary

--- pluginimpl/tl1Performance/templatecreationfromRequirementsdoc.py
+++ pluginimpl/tl1Performance/templatecreationfromRequirementsdoc.py
@@ -4,15 +4,25 @@ __status__ = 'Development'
 from metricsFromRequirementsdoc import parseexcel
 import re
 
-sheet = parseexcel("/home/sthummala/Downloads/SmartPlugin-TestSheet-fujitsu-flashwave-9500.xlsx")
+sheet = parseexcel("/home/sthummala/Downloads/SmartPlugin-TestSheet-fujitsu-flashwave-9500.xlsx")  # Loading the excel sheet
 print(len(sheet.pmsheets))
 for i in sheet.pmsheets:
     f = open("templates/fujitsu-fw-9500-"+i+"-tl1.xml",'w')
     f.write('<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE objects PUBLIC "pm/templates/pmTemplate.dtd" "pmTemplate.dtd">\n\n\n<objects version="UNCLEAN">\n\n  <perf-template name="fujitsu-fw-9500-{}-tl1">\n\n    <description>{} stats</description>\n\n    <templateType>PLUGIN</templateType>\n    <pm-metric-groups type="String">\n'.format(i,i))
+    # Passing the sheet name one at a time to fetch the details of montypes
+
+    sheet.clearmontypedict()  # clearing the previous dict value to not save previous sheets value
     sheet.getmontypedetails(i,['Montype', 'Direction', 'Location', 'Binned', 'Nonbinned', '', 'Model', 'Enable', 'Metric', 'Units', 'Type', '', 'Verification Date', 'History PM', 'Live PM', '', 'Verification Date', 'History PM', 'Live PM'])
-    print("-------------------------------------------------------------\n"+i+"\n----------------------------------------------------")
-    for j in set(sheet.temp[2::]):
+
+    montypes = sheet.sheetmontypedict
+    print(montypes)
+    # print("-------------------------------------------------------------\n"+i+"\n----------------------------------------------------")
+    for j in set([x[0] for _, x in montypes.items()]):
         if len(j) > 1:
+            if j == 'Montype':
+                continue
+            if "where" in j:
+                continue
             if not j.startswith('<'):
                 f.write("\t\t<value>{} Statistics.TL1</value>\n".format(re.sub('<.*','',j)))
                 print(j)
