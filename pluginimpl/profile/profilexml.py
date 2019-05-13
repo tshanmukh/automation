f0071fa78ce1242052772efda48cdc72e75c6209
tshanmukh <Shanmukh@1996>
2019-05-13 14:08:53 +0530

    Updated the profilexml class to take repopath as input and return full file paths

--- pluginimpl/profile/profilexml.py
+++ pluginimpl/profile/profilexml.py
@@ -1,15 +1,14 @@
 __author__ = 'shanmukh'
 __status__ = 'Development'
 
-import os
 from lxml import etree
-import pickle
-import glob
+import utils
 
 class profilexml():
-    def __init__(self,profilexml):
+    def __init__(self,profilexml,repopath):
         self.profilexml = profilexml
-        self.profile = repopath + 'centina/sa/profiles/' + pluginName + '.xml'
+        self.repopath = repopath
+        self.profile = self.repopath + 'centina/sa/profiles/' + self.profilexml + '.xml'
         parser = etree.XMLParser(strip_cdata=False)
         self.root = etree.parse(self.profile, parser)
         self.pmtemplatepaths = []
@@ -45,19 +44,22 @@ class profilexml():
             if a.get("path").startswith("pm/templates"):
                 if a.get("path").endswith(".dtd"):
                     continue
-                pmtemplate = repopath + "centina/sa/profiles/" + a.get("path")
+                pmtemplate = self.repopath + "centina/sa/profiles/" + a.get("path")
                 # print("Found pm template ", pmtemplate)
                 self.pmtemplatepaths.append(pmtemplate)
 
-
+            # loading the pmfilepaths
             elif a.get("path").startswith("pm/"):
                 # todo save all the other paths
                 if not a.get("path").endswith(".dtd"):
-                    self.pmfilepaths.append(a.get("path"))
+                    self.pmfilepaths.append(self.repopath + "centina/sa/profiles/"+a.get("path"))
 
+            # loading inventory paths
             elif a.get("path").startswith("snmp/inventory/"):
                 if not a.get("path").endswith(".dtd"):
                     self.inventoryfilepaths.append(a.get("path"))
+
+            # loading trap paths
             elif a.get("path").startswith("snmp/trap/"):
                 if not a.get("path").endswith(".dtd"):
                     self.trapfilepaths.append(a.get("path"))
@@ -67,7 +69,7 @@ class profilexml():
         founddict = {}
         if self.pmfilepaths:
             for i in self.pmfilepaths:
-                filePath = repopath + "centina/sa/profiles/" + i
+                filePath = self.repopath + "centina/sa/profiles/" + i
                 ref = i.split("/")
                 reference = ref[1].replace(".xml", "")
                 parser = etree.XMLParser(strip_cdata=False)
@@ -85,45 +87,4 @@ class profilexml():
 
 
 if __name__ == '__main__':
-    pmFiles = []
-    metricGroupId = []
-    # pickling the repo
-    repopath = None
-    if os.path.isfile("../.repopath.pickle"):
-        infile = open("../.repopath.pickle", 'rb')
-        new_dict = pickle.load(infile)
-        infile.close()
-        repopath = new_dict.get("repository")
-        print("Repository path saved is {}".format(repopath))
-    else:
-        repopath = input("Enter the path to the repository: ")
-        variable = {"repository": repopath}
-        f = open(".repopath.pickle", 'wb')
-        pickle.dump(variable, f)
-        f.close()
-
-    files = glob.glob(repopath+"centina/sa/profiles/*.xml")
-    xmls = [f.split('/')[-1].replace('.xml','') for f in files]
-
-    # for pluginName in xmls:
-        # pluginName = input("Please enter the plugin name: ")
-    pluginName = 'opterna-am-omni2000'
-        # plugin = pluginName.split('/')[-1].replace('.xml','')
-    profile = profilexml(pluginName)
-
-    # print(profile.pmtemplatepaths)
-    # print(" the metric groupid's are {}".format(profile.parsetemplates(profile.pmtemplatepaths[0])))
-    # print(profile.pmtemplatepaths[0],profile.parsetemplates(profile.pmtemplatepaths[0]))
-    show = True
-    for i in profile.pmtemplatepaths:
-
-        metricgroupids = profile.parsetemplates(i)
-        found,founddict = profile.checkmetricgroupids(metricgroupids)
-
-        if bool(set(metricgroupids)-found):
-            if show:
-                print("---------------------------------------------------------------------------------------------")
-                print("plugin: "+pluginName + "--"+profile.protocol)
-                print("---------------------------------------------------------------------------------------------")
-                show = False
-            print("Template: "+str(i.split('/')[-1])+" Missing id's "+str(set(metricgroupids)-found))
+    repopath = utils.getrepopath()
