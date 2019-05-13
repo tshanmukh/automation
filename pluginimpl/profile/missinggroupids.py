0b055984589ada69b4bfaff334dc536be8921344
tshanmukh <Shanmukh@1996>
2019-05-13 14:08:06 +0530

    Script to check if any metric group id's are misssing in a plugin

--- pluginimpl/profile/missinggroupids.py
+++ pluginimpl/profile/missinggroupids.py
@@ -1,24 +1,11 @@
 import glob
-import pickle
-import os
 from profilexml import profilexml
+import utils
 
 pmFiles = []
 metricGroupId = []
 # pickling the repo
-repopath = None
-if os.path.isfile("../.repopath.pickle"):
-    infile = open("../.repopath.pickle", 'rb')
-    new_dict = pickle.load(infile)
-    infile.close()
-    repopath = new_dict.get("repository")
-    print("Repository path saved is {}".format(repopath))
-else:
-    repopath = input("Enter the path to the repository: ")
-    variable = {"repository": repopath}
-    f = open("../.repopath.pickle", 'wb')
-    pickle.dump(variable, f)
-    f.close()
+repopath = utils.getrepopath()
 
 files = glob.glob(repopath + "centina/sa/profiles/*.xml")
 xmls = [f.split('/')[-1].replace('.xml', '') for f in files]
